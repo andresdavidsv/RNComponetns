30bc3f9f7c9898ca93098574c11780837ec32ae1
andresdavidsv <soy@andresdavidsv.com>
2023-04-07 17:56:35 -0500

    feat: convert switch to custom component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {View, FlatList} from 'react-native';
 
 import {menuItems} from '../data/menuItems';
 import {styles} from '../theme/appTheme';
 import {FlatListMenuItem} from '../components/FlatListMenuItem';
+import {HeaderTitle} from '../components/HeaderTitle';
 
 export const HomeScreen = () => {
-  const {top} = useSafeAreaInsets();
-
-  const renderListHeader = () => {
-    return (
-      <View style={{marginTop: top + 20, marginBottom: 20}}>
-        <Text style={styles.title}> Menu Options </Text>
-      </View>
-    );
-  };
-
   const itemSeparetor = () => {
     return (
       <View style={{borderBottomWidth: 1, opacity: 0.4, marginVertical: 8}} />
@@ -29,7 +19,7 @@ export const HomeScreen = () => {
         data={menuItems}
         renderItem={({item}) => <FlatListMenuItem menuItem={item} />}
         keyExtractor={item => item.name}
-        ListHeaderComponent={() => renderListHeader()}
+        ListHeaderComponent={() => <HeaderTitle title="Menu Options" />}
         ItemSeparatorComponent={() => itemSeparetor()}
       />
     </View>
--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,18 +1,61 @@
 import React, {useState} from 'react';
-import {View, Switch, Platform} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {CustomSwitch} from '../components/CustomSwitch';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const {isActive, isHappy, isHungry} = state;
+
+  const onChange = (value: boolean, field: keyof typeof state) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
+
   return (
-    <View style={{marginTop: 100}}>
-      <Switch
-        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
-        thumbColor={Platform.OS === 'android' ? '#5856d6' : ''}
-        ios_backgroundColor=""
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+    <View style={{marginHorizontal: 20}}>
+      <HeaderTitle title="Switches" />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isActive</Text>
+        <CustomSwitch
+          isOn={isActive}
+          onChange={value => onChange(value, 'isActive')}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomSwitch
+          isOn={isHungry}
+          onChange={value => onChange(value, 'isHungry')}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomSwitch
+          isOn={isHappy}
+          onChange={value => onChange(value, 'isHappy')}
+        />
+      </View>
+      <Text style={styles.switchText}>{JSON.stringify(state, null, 5)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
