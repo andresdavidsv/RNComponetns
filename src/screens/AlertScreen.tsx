d4d7ca74e92ad04d853f17bc982c579efbf5614b
andresdavidsv <soy@andresdavidsv.com>
2023-04-08 09:05:16 -0500

    feat: add input and validate form

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button, View, Alert} from 'react-native';
+import prompt from 'react-native-prompt-android';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {styles} from '../theme/appTheme';
 
@@ -14,10 +15,42 @@ export const AlertScreen = () => {
       {text: 'OK', onPress: () => console.log('OK Pressed')},
     ]);
   };
+  const showPrompt = () => {
+    // Alert.prompt(
+    //   'Are you ok?',
+    //   'These actions are irreversible',
+    //   (value: string) => console.log({value}),
+    //   'plain-text',
+    //   'Hello World',
+    // );
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
   return (
     <View style={styles.globalmargin}>
       <HeaderTitle title="Alert" />
+      <View style={{height: 10}} />
       <Button title="Show Alert" onPress={() => showAlert()} />
+      <Button title="Show Prompt" onPress={() => showPrompt()} />
     </View>
   );
 };
---
+++ src/screens/TextInputScreen.tsx
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+  Text,
+} from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import {useForm} from '../hooks/useForm';
+import {CustomSwitch} from '../components/CustomSwitch';
+
+export const TextInputScreen = () => {
+  const {form, onChange, isSubscribed} = useForm({
+    name: '',
+    email: '',
+    phone: '',
+    isSubscribed: false,
+  });
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.globalmargin}>
+            <HeaderTitle title="Text Input" />
+            <TextInput
+              style={stylesScreen.textInput}
+              placeholder="Input Name"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange(value, 'name')}
+            />
+            <TextInput
+              style={stylesScreen.textInput}
+              placeholder="Input Email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={value => onChange(value, 'email')}
+              keyboardType="email-address"
+            />
+            <View style={stylesScreen.switchRow}>
+              <Text style={stylesScreen.switchText}>Subscribe</Text>
+              <CustomSwitch
+                isOn={isSubscribed}
+                onChange={value => onChange(value, 'isSubscribed')}
+              />
+            </View>
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <TextInput
+              style={stylesScreen.textInput}
+              placeholder="Input Phone"
+              onChangeText={value => onChange(value, 'phone')}
+              keyboardType="phone-pad"
+            />
+          </View>
+          <View style={{height: 100}} />
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const stylesScreen = StyleSheet.create({
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.3)',
+    height: 50,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
